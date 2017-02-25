ec43a8c24e4eee4acd26a2d172779dfde1c58482
dylandd13@outlook.com <dylan@LeLinux.localdomain>
2017-02-25 00:53:20 -0400

    Firebase intialized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
+import {AngularFireModule}  from 'angularfire2';
 import { AppComponent } from './app.component';
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyCvjY_1pbNlPw0PoGjHxOjSxTcu0gug09M',
+  authDomain: 'sjapp-4c72a.firebaseapp.com',
+  databaseURL: 'https://sjapp-4c72a.firebaseio.com',
+  storageBucket: 'sjapp-4c72a.appspot.com',
+  messagingSenderId: '242489775623'
+};
+
 
 @NgModule({
   declarations: [
@@ -12,7 +21,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
