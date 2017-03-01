61a1ec253e95be1de931c05cdb794392157cc4d3
dylandd13@outlook.com <dylan@LeLinux.localdomain>
2017-02-28 23:14:58 -0400

    AuthGuard Fixed, Article Pull added

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,9 @@
  * Created by dylan on 2/26/17.
  */
 import { Component } from '@angular/core';
+import { AngularFire , FirebaseListObservable } from 'angularfire2';
+import {Router} from "@angular/router";
+
 
 
 @Component({
@@ -9,4 +12,31 @@ import { Component } from '@angular/core';
   templateUrl: './dashboard.component.html'
 })
 
-export class DashboardComponent { }
+export class DashboardComponent {
+
+  articles: FirebaseListObservable<any>;
+  constructor(private af: AngularFire, private router: Router) {
+   this.af.auth.subscribe(authData => {
+     console.log(authData);
+     let uid=authData.uid;
+     this.articles=this.af.database.list('/ARTICLES', {
+       query: {
+         orderByChild: 'authorUID',
+         equalTo: uid
+       }
+     })
+     this.articles.subscribe(
+       val => console.log(val)
+     );
+     }
+
+   );
+  }
+
+  logout() {
+    this.af.auth.logout().then(
+      (success) => {
+        this.router.navigate(['/login']);
+      })
+  }
+}
--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,19 +3,24 @@
  */
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { DashboardComponent } from './dashboard.component';
 import { MaterialModule } from '@angular/material';
 import { DashRoutingModule} from './dashboard-routing.module';
 import {AuthGuard} from "../auth/auth-guard.service";
 
+import { LoginModule } from 'app/login/login.module';
+
+
+
 @NgModule({
   imports:[
-
+    BrowserModule,
     FormsModule,
     MaterialModule,
-    //DashRoutingModule
+    //DashRoutingModule,
+
       ],
 
   exports: [
