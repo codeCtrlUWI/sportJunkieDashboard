0dc923bf23ef1645e98c2386b57e13633ef11e62
codeCtrlUWI <infocodectrl@gmail.com>
2017-02-28 23:17:08 -0400

    Add files via upload

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,11 @@
 import { Component } from '@angular/core';
-import { AngularFire} from 'angularfire2';
-import {FirebaseListObservable} from 'angularfire2';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-
   styleUrls: ['./app.component.css']
 })
-
-
 export class AppComponent {
-  items: FirebaseListObservable<any>;
-
-
-constructor(public af: AngularFire){
-  this.items= af.database.list('/Article')
-
-  this.items.subscribe(
-    val=> console.log(val)
-  );
-
-
-}
-
-
-
-
+  title = 'Sports Junkie Author Dashboard';
 }
